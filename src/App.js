2660a0ee9a462c88538609b5ad1f4ffa6097cad4
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-20 16:23:54 +0600

    recape fundamental for react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
     </div>
   );
 }
-
+function LoadComments(){
+  const [comments, setComments] = useState([]);
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/comments")
+    .then(res => res.json())
+    .then(data => setComments(data))
+  },[])
+  return (
+    <div>
+      <h3>{comments.length}</h3>
+      {
+        comments.slice(0,10).map((comment)=> <Comment email={comment.email} body={comment.body}></Comment>)
+      }
+    </div>
+  )
+}
+function Comment(props){
+  return (
+    <div>
+      <h3 >{props.email}</h3>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+function Counter(){
+  const [count, setCount] = useState(55)
+  const incraseCount = ()=> setCount(count + 1);
+  const handalDecrase = ()=> setCount(count - 1);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button style={{margin: '0 20px'}} onClick={incraseCount}>Incrase+</button>
+      <button onClick={handalDecrase}>Decrase-</button>
+    </div>
+  );
+}
 export default App;
